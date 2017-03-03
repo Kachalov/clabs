cd84b422823662d64d4246a1216ba5141e9e1f9d
Kachalov Alexey <kachalov@kistriver.com>
2017-03-03 21:06:12 +0300

    Added task 1

---
+++ lab_01_01/1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define PI 3.1415926535897932384626433832795
+
+int main(void)
+{
+    float a, b, h, alpha;
+    float square;
+
+    printf("Enter a, b, alpha: ");
+    scanf("%f %f %f", &a, &b, &alpha);
+
+    alpha = alpha/180.0*PI;
+    h = labs(a-b)/2.0*tan(alpha);
+
+    // a == b or alpha > pi/2
+    if(h <= 0)
+    {
+        printf("Invalid parameters\n");
+        return 1;
+    }
+
+    square = (a+b)/2.0*h;
+    printf("Square: %.3f\n", square);
+
+    return 0;
+}
