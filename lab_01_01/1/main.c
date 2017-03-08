4ebf301368cf6d2a8c566da0a46ec7860b7cbb87
Kachalov Alexey <kachalov@kistriver.com>
2017-03-08 21:11:02 +0300

    Appearance and OR operator usage were fixed.

--- lab_01_01/1/main.c
+++ lab_01_01/1/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
 #define PI 3.1415926535897932384626433832795
 
@@ -12,17 +11,17 @@ int main(void)
     printf("Enter a, b, alpha: ");
     scanf("%f %f %f", &a, &b, &alpha);
 
-    alpha = alpha/180.0*PI;
-    h = fabs(a-b)/2.0*tan(alpha);
+    alpha = alpha / 180.0 * PI;
+    h = fabs(a-b) / 2.0 * tan(alpha);
 
     // a == b or alpha > pi/2
-    if(h <= 0)
+    if (h <= 0)
     {
         printf("Invalid parameters\n");
         return 1;
     }
 
-    square = (a+b)/2.0*h;
+    square = (a + b) / 2.0 * h;
     printf("Square: %.3f\n", square);
 
     return 0;
--- lab_01_01/3/main.c
+++ lab_01_01/3/main.c
@@ -8,14 +8,14 @@ int main(void)
     printf("Input v1, t1, v2, t2: ");
     scanf("%f %f %f %f", &v1, &t1, &v2, &t2);
 
-    if((v1 < 0) | (v2 < 0))
+    if ((v1 < 0) || (v2 < 0))
     {
-        printf("Invalid V\n");
+        printf("Invalid volumes\n");
         return 1;
     }
 
     v0 = v1 + v2;
-    t0 = (t1*v1 + t2*v2)/v0;
+    t0 = (t1 * v1 + t2 * v2) / v0;
 
     printf("v0: %.3f; t0: %.3f\n", v0, t0);
     return 0;
