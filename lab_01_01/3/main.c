ddba9af0ab8b43bf226eb866efbe45fc892b717c
Kachalov Alexey <kachalov@kistriver.com>
2017-03-17 21:42:26 +0300

    Temperature checking was added.

--- lab_01_01/3/main.c
+++ lab_01_01/3/main.c
@@ -5,7 +5,7 @@ int main(void)
     float v0, v1, v2;
     float t0, t1, t2;
 
-    printf("Input v1, t1, v2, t2: ");
+    printf("Input v1, t1, v2, t2 (m^3 and K): ");
     scanf("%f %f %f %f", &v1, &t1, &v2, &t2);
 
     if ((v1 < 0) || (v2 < 0))
@@ -14,9 +14,15 @@ int main(void)
         return 1;
     }
 
+    if ((t1 < 0) || (t2 < 0))
+    {
+        printf("Invalid temperatures\n");
+        return 1;
+    }
+
     v0 = v1 + v2;
     t0 = (t1 * v1 + t2 * v2) / v0;
 
-    printf("v0: %.3f; t0: %.3f\n", v0, t0);
+    printf("v0: %.3f m^3\nt0: %.3f K\n", v0, t0);
     return 0;
 }
