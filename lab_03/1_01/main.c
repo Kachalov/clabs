92cf6f64ac01ea7b978f15d82353fcc9f7d2ce66
Kachalov Alexey <kachalov@kistriver.com>
2017-04-15 12:41:00 +0400

    Added task 1

---
+++ lab_03/1_01/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define ERR_NO_FILE -1
+#define ERR_NOT_ENOUGH_ELEMENTS -2
+
+int process(FILE *f, float *max1, float *max2);
+
+int main(void)
+{
+    int status;
+    float max1, max2;
+    status = process(stdin, &max1, &max2);
+
+    switch (status)
+    {
+        case 0:
+        printf("%f %f\n", max1, max2);
+        break;
+    case ERR_NO_FILE:
+        printf("No file found");
+        break;
+    case ERR_NOT_ENOUGH_ELEMENTS:
+        printf("Not enough elements\n");
+        break;
+    default:
+        printf("Unknown error: %d", status);
+        break;
+    }
+    return status;
+}
+
+int process(FILE *f, float *max1, float *max2)
+{
+    float x;
+    unsigned int i = 0;
+
+
+    if (f == NULL)
+    {
+        return ERR_NO_FILE;
+    }
+
+    while (scanf("%f", &x))
+    {
+        if (*max1 <= x || i == 0)
+        {
+            *max2 = *max1;
+            *max1 = x;
+        }
+        i++;
+    }
+
+    if (i < 2)
+    {
+        return ERR_NOT_ENOUGH_ELEMENTS;
+    }
+
+    return 0;
+}
