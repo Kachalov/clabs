6de0e133b209eab074df921e47e2e0b1a5d35909
Kachalov Alexey <kachalov@kistriver.com>
2017-05-24 00:07:32 +0300

    Added implementaion of del_negative function. Added array_mutable_function_t type.

--- lab_04/main.c
+++ lab_04/main.c
@@ -11,6 +11,7 @@
 #define ERR_NO_DATA 3
 
 typedef int (*array_function_t)(int *, int);
+typedef int (*array_mutable_function_t)(int *, int *);
 
 int read_array(int *arr, int *arr_size);
 void print_array(int *const arr, int arr_size);
@@ -18,20 +19,26 @@ void print_error(int err, int err_function);
 
 int sum_mul(int *const arr, int arr_size);
 int copy_avg(int *const arr, int arr_size);
-int del_negative(int *const arr, int arr_size);
-int add_sum(int *const arr, int arr_size);
-int swap(int *const arr, int arr_size);
-int replace_max(int *const arr, int arr_size);
-int sort_min_max(int *const arr, int arr_size);
+int del_negative(int *arr, int *arr_size);
+int add_sum(int *arr, int *arr_size);
+int swap(int * arr, int arr_size);
+int replace_max(int * arr, int arr_size);
+int sort_min_max(int * arr, int arr_size);
 
 int main(void)
 {
     int arr[ARRAY_SIZE] = {0};
     int arr_size = 0;
+
+    int arr_tmp[ARRAY_SIZE] = {0};
+    int arr_tmp_size = 0;
+
     int err = OK;
     int err_function = -1;
-    array_function_t funs[] = {&sum_mul, &copy_avg, &del_negative, &add_sum, 
-        &swap, &replace_max, &sort_min_max};
+
+    array_function_t funs[] = {&sum_mul, &copy_avg, &swap, &replace_max,
+                               &sort_min_max};
+    array_mutable_function_t funs_mutable[] = {&del_negative, &add_sum};
 
     if((err = read_array(arr, &arr_size)) != OK)
         goto fail;
@@ -41,6 +48,20 @@ int main(void)
         err_function = i + 1;
         if ((err = (*funs[i])(arr, arr_size)) != OK)
             print_error(err, err_function);
+    }
+
+    for (int i = 0;
+         i < sizeof(funs_mutable)/sizeof(array_mutable_function_t); i++)
+    {
+        err_function = i + 1;
+
+        memcpy(arr_tmp, arr, sizeof(arr));
+        arr_tmp_size = arr_size;
+        if ((err = (*funs_mutable[i])(arr_tmp, &arr_tmp_size)) != OK)
+            print_error(err, err_function);
+        else
+            printf("Function #%d array(%d): ", i + 1, arr_tmp_size);
+            print_array(arr_tmp, arr_tmp_size);
     }
 
     fail:
@@ -150,28 +171,47 @@ int copy_avg(int *const arr, int arr_size)
     return OK;
 }
 
-int del_negative(int *const arr, int arr_size)
+int del_negative(int *arr, int *arr_size)
+{
+    int offset = 0;
+
+    if (*arr_size == 1 && arr[0] < 0)
+    {
+        (*arr_size)--;
+        arr[0] = 0;
+    }
+
+    for (int i = 1; i < *arr_size; i++)
+    {
+        arr[i - offset] = arr[i];
+
+        if (arr[i - 1 - offset] >= 0)
+            continue;
+
+        arr[i - 1 - offset++] = arr[i];
+        arr[i] = 0;
+    }
+    *arr_size -= offset;
+
+    return OK;
+}
+
+int add_sum(int * arr, int *arr_size)
 {
     return OK;
 }
 
-int add_sum(int *const arr, int arr_size)
+int swap(int * arr, int arr_size)
 {
     return OK;
 }
 
-int swap(int *const arr, int arr_size)
+int replace_max(int * arr, int arr_size)
 {
     return OK;
 }
 
-int replace_max(int *const arr, int arr_size)
+int sort_min_max(int * arr, int arr_size)
 {
     return OK;
 }
-
-int sort_min_max(int *const arr, int arr_size)
-{
-    return OK;
-}
-
