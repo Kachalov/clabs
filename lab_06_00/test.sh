b3e8b62cab40ed86e1045ecfadd5a79bfe489798
Kachalov Alexey <kachalov@kistriver.com>
2017-06-02 22:22:35 +0300

    Added coverage testing and new tests.

--- lab_06_00/test.sh
+++ lab_06_00/test.sh
@@ -5,6 +5,12 @@ for i in $(find . -name 'in_*.txt'); do
     diff -su $(echo $i | sed 's/in/out/g') last_test.txt
 done
 
+./lab.bin in_6.txt > last_test.txt 2>&1
+diff -su out_6.txt last_test.txt
+
+./lab.bin > last_test.txt 2>&1
+diff -su out_7.txt last_test.txt
+
 if [ -f last_test.txt ]; then
     rm last_test.txt
 fi
