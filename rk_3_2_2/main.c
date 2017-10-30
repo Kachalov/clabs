2b0b1decd2a0d8e5a09baf57258846cf80f44672
Alexey <kachalov@kistriver.com>
2017-10-30 11:36:38 +0300

    Replaced tabs with spaces

--- rk_3_2_2/main.c
+++ rk_3_2_2/main.c
@@ -7,89 +7,90 @@
 
 int main(int argc, char **argv)
 {
-	FILE *f = NULL;
-	char buf[MAX_BUF];
-	char *cur = NULL;
-	char *end = NULL;
-	double x = 0;
+    FILE *f = NULL;
+    char buf[MAX_BUF];
+    char *cur = NULL;
+    char *end = NULL;
+    double x = 0;
         bool naned = false;
 
-	if (argc > 1)
-	{
-		f = fopen(argv[1], "r");
-		if (f != NULL)
-		{
-                        int j = 0;
-                        while (fgets(buf, MAX_BUF, f))
-			{
-                                j++;
-                                int i = 0;
-                                cur = buf;
-                                //printf("While '%s' %d:\n", buf, j);
-				do
-				{
-                                    i++;
-                                    x = strtod(cur, &end);
-                                    //printf("S '%s'\n", cur);
-                                    if (cur == end || ( !(
-                                                           *end == '\r'
-                                                           || *end == '\n'
-                                                           || *end == ' ')))
-                                    {
-                                            //printf("true %p %p (%c)\n", cur, end, *cur);
-                                            while ('0' <= *cur && *cur <= '9')
-                                            {
-                                                if (!naned)
-                                                    printf("nan ");
-                                                naned = true;
-                                                cur++;
-                                            }
-                                            while (!(('0' <= *cur && *cur <= '9')
-                                                     || *cur == '.'
-                                                     || *cur == '-'
-                                                     || *cur == '\0')
-                                                   )
-                                            {
-                                                    if (*cur == '\r'
-                                                            || *cur == '\n')
-                                                    {
-                                                        printf("\n");
-                                                    }
-                                                    else if (!(('0' <= *cur && *cur <= '9')
-                                                               || *cur == '.'
-                                                               || *cur == '-'))
-                                                    {
-                                                        if (!naned)
-                                                            printf("nan ");
-                                                        naned = true;
-                                                    }
-                                                    //printf("i");
-                                                    cur++;
-                                            }
-                                            naned = false;
-                                            //printf("\n");
-                                    }
-                                    else
-                                    {
-                                        printf("%.2f ", x);
-                                        cur = end;
-                                    }
-                                    //printf("%p %p\n", cur, buf + MAX_BUF - 1);
-				}
-                                while (cur != buf + MAX_BUF && i < 10);
-                                strcpy(buf, "");
-			}
-		}
-		else
-		{
-			printf("File error\n");
-		}
-	}	
-	else
-	{
-		printf("No filename");
-	}
+    if (argc > 1)
+    {
+        f = fopen(argv[1], "r");
+        if (f != NULL)
+        {
+            int j = 0;
+            while (fgets(buf, MAX_BUF, f))
+            {
+                j++;
+                int i = 0;
+                cur = buf;
+                //printf("While '%s' %d:\n", buf, j);
+                do
+                {
+                    i++;
+                    x = strtod(cur, &end);
+                    //printf("S '%s'\n", cur);
+                    if (cur == end
+                            || ( !(
+                               *end == '\r'
+                               || *end == '\n'
+                               || *end == ' ')))
+                    {
+                        //printf("true %p %p (%c)\n", cur, end, *cur);
+                        while ('0' <= *cur && *cur <= '9')
+                        {
+                            if (!naned)
+                                printf("nan ");
+                            naned = true;
+                            cur++;
+                        }
+                        while (!(('0' <= *cur && *cur <= '9')
+                                 || *cur == '.'
+                                 || *cur == '-'
+                                 || *cur == '\0')
+                               )
+                        {
+                            if (*cur == '\r'
+                                    || *cur == '\n')
+                            {
+                                printf("\n");
+                            }
+                            else if (!(('0' <= *cur && *cur <= '9')
+                                       || *cur == '.'
+                                       || *cur == '-'))
+                            {
+                                if (!naned)
+                                    printf("nan ");
+                                naned = true;
+                            }
+                            //printf("i");
+                            cur++;
+                        }
+                        naned = false;
+                        //printf("\n");
+                    }
+                    else
+                    {
+                        printf("%.2f ", x);
+                        cur = end;
+                    }
+                    //printf("%p %p\n", cur, buf + MAX_BUF - 1);
+                }
+                while (cur != buf + MAX_BUF && i < 10);
+                strcpy(buf, "");
+            }
+        }
+        else
+        {
+            printf("File error\n");
+        }
+    }
+    else
+    {
+        printf("No filename");
+    }
 
-	return 0;
+    return 0;
 }
 
