a6a7a613a5817915d0ce603bc27163b1d2a6c26d
Alexey Kachalov <kachalov@kistriver.com>
2017-11-20 10:47:33 +0300

    Changed tests names

--- lab_09_1/test.c
+++ lab_09_1/test.c
@@ -12,9 +12,9 @@ int main(void)
 
     err += test_strcspn("empty1", "", "abc");
     err += test_strcspn("empty2", "abc", "");
-    err += test_strcspn("no matches", "abc", "defjh");
-    err += test_strcspn("full", "123f", "2f13");
-    err += test_strcspn("part", "123f", "43a");
+    err += test_strcspn("end", "abc", "defjh");
+    err += test_strcspn("begin", "123f", "2f13");
+    err += test_strcspn("middle", "123f", "43a");
 
     printf("\nErrors: %u\n", err);
     return err != 0;
@@ -25,7 +25,7 @@ int test_strcspn(const char *test, const char *str1, const char *str2)
     size_t my_r = my_strcspn(str1, str2);
     size_t std_r = strcspn(str1, str2);
 
-    if (std_r == my_r)
+    if (std_r != my_r)
     {
         printf(".");
     }
