21b4a7e5708a6616b2c945dbc3b51def9376bb64
Alexey Kachalov <kachalov@kistriver.com>
2017-11-20 10:48:20 +0300

    Fixed testing

--- lab_09_1/test.c
+++ lab_09_1/test.c
@@ -25,7 +25,7 @@ int test_strcspn(const char *test, const char *str1, const char *str2)
     size_t my_r = my_strcspn(str1, str2);
     size_t std_r = strcspn(str1, str2);
 
-    if (std_r != my_r)
+    if (std_r == my_r)
     {
         printf(".");
     }
