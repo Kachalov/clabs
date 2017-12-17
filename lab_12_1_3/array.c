0ca1578018deaa1bd5abaabfeb7911b23a24553e
Kachalov Alexey <kachalov@kistriver.com>
2017-12-17 23:51:16 +0300

    Lab 7 in lab 12.1

---
+++ lab_12_1_3/array.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "array.h"
+#include "errors.h"
+
+void print_int_array(FILE *fd, const void *b, const void *e)
+{
+    for(int *it = (int *)b; it < (int *)e; it++)
+    {
+        fprintf(fd, "%d ", *it);
+    }
+    fprintf(fd, "\n");
+}
+
+int create_array_file(FILE *fd, size_t num, size_t size, void **data)
+{
+    int err = OK;
+    void *it = NULL;
+    int tmp = 0;
+
+    fseek(fd, 0, SEEK_SET);
+    if ((err = create_array(num, size, data)) == OK) {
+        it = *data;
+
+        while (fscanf(fd, "%d", &tmp) == 1) {
+            memcpy(it, &tmp, size);
+            it = (char *) it + size;
+        }
+
+        fseek(fd, 0, SEEK_SET);
+    }
+    return err;
+}
+
+int create_array(size_t num, size_t size, void **data)
+{
+    *data = malloc(num * size);
+    if (*data == NULL)
+        return MEM_ALLOC_ERR;
+
+    return OK;
+}
+
+void delete_array(void **data)
+{
+    free(*data);
+    *data = NULL;
+}
---
+++ lab_12_1_3/sort.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "stdlib.h"
+
+typedef int (*cmp_f_t)(const void *, const void *);
+
+void sort(void *data, size_t num, size_t size, cmp_f_t cmp_f);
+
+int cmp_int(const void *a, const void *b);
